74385fd155a745cc94de5a5b914d3cb98ca7916f
GodHelpMe001 <113171459+GodHelpMe001@users.noreply.github.com>
2025-04-25 00:38:33 +0800

    serialization utilities + entt library

--- Headers/arch/common/component.h
+++ Headers/arch/common/component.h
@@ -12,7 +12,9 @@ public:
 	// not intended to be set outside.
 	void SetComponentID(ComponentType _type)	{ m_type = _type; }
 	ComponentType GetComponentID() const		{ return m_type; }
-
+	
+	// serialization function
+	static void Register() {};
 private:
 	ComponentType m_type						{ __INVALID };
 
